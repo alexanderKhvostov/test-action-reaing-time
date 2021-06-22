ba5f68fb4315b966e5524fb1587987ab163f261c
alexandr khvostov <alexandr.khvostov@sysgears.com>
2021-06-22 13:38:42 +0300

    test(main): fixes

--- src/calculateRoutesReadingTime.mjs
+++ src/calculateRoutesReadingTime.mjs
@@ -1,13 +1,13 @@
 import readingTime from 'reading-time'
-import { getFile } from './github.mjs'
+// import { getFile } from './github.mjs'
 
 export default function (routes) {
   return routes.map((mainRoute) => {
     return {
       ...mainRoute,
       routes: mainRoute.routes.map(async (subRoute) => {
-        const { content } = await getFile(subRoute.path)
-        return { ...subRoute, readingTime: readingTime(content) }
+        // const { content } = await getFile(subRoute.path)
+        return { ...subRoute, readingTime: readingTime('') }
       })
     }
   })
