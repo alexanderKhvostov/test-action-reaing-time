aeffd91b72e8ce6ff3c64c6011b6af534dc96014
alexandr khvostov <alexandr.khvostov@sysgears.com>
2021-06-22 13:40:10 +0300

    test(main): fixes

--- src/manifestHelpers.mjs
+++ src/manifestHelpers.mjs
@@ -1,6 +1,8 @@
 import { getFile } from './github.mjs'
+import core from '@actions/core'
 
 export async function fetchDocsManifest(path) {
+  core.info(path)
   const res = await getFile(path)
   return JSON.parse(res)
 }
